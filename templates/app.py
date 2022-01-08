8a2ae4c313dbed372ff3d9add3909edae29bb5d3
nladkins <adkins.norman@epa.gov>
2022-01-08 14:45:48 -0500

    Update to app.py

--- templates/app.py
+++ templates/app.py
@@ -1,42 +1,77 @@
-from flask import Flask
-from flask import render_template # to render the error page
-import psycopg2
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+path = (f"postgresql://postgres:{password}@localhost:5432/neuro_disorder_db")
+engine = create_engine(path, echo=True)
+Base.metadata.create_all(engine)
+
+#################################################
+# Database Setup
+#################################################
+
+path = (f"postgresql://postgres:{password}@localhost:5432/neuro_disorder_db")
+engine = create_engine(path, echo=True)
+Base = automap_base()
+Base.metadata.create_all(engine)
+# Save reference to the table
+disorder = Base.classes.disorders
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+
 @app.route("/")
+def welcome():
+    """Connecting."""
+    return (
+        f"Available Routes:<br/>"
+        f"neuro_disorder_db/code<br/>"
+        f"neuro_disorder_db/passengers"
+    )
 
-host = "localhost"
-port = "5432"
-dbname = "neuro_disorder_db"
-user = "admin"
-password = "postgres"
-db_conn = psycopg2.connect(host=host, port=port, dbname=dbname, user=user, password=password)
-db_cursor = db_conn.cursor()
+@app.route("/neuro_disorder_db/disorders")
+def disorders():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-@app.route("/import")
-def csv_import():
-    # Trap errors for opening the file
-    try:
-        file_path = "clean-data/neuro_disorder.csv"
-        contents = open(file_path, 'r')
-    except psycopg2.Error as e:
-        message = "Database error: " + e + "/n open() text file: " + file_path
-        return render_template("error_page.html", message = message)
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(disorders.Entity, disorders.Code, disorders.Year, disorders.Prevelance_in_Males, disorders.Prevelance_in_Females, disorders.Population. disorders.Continent).all()
 
-    # Trap errors for copying the array to our database
-    try:
-        # Is this really needed??  Found verbage online about this.  If we are using the admin.
-        db_cursor.copy_from(contents, "tbl_users", columns=('t_name_user', 't_email'), sep=",")
-    except psycopg2.Error as e:
-        message = "Database error: " + e + "/n copy_from"
-        return render_template("error_page.html", message = message)
+    session.close()
 
-    # It got this far: Success!
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_disorders = []
+    for entity, code, year, prevelance_in_males, prevelance_in_females, population, continent in results:
+        disorder_dict = {}
+        disorder_dict["Entity"] = entity
+        disorder_dict["Code"] = code
+        disorder_dict["Year"] = year
+        disorder_dict["Prevelance_in_Males"] = prevelance_in_males
+        disorder_dict["Prevelance_in_Females"] = prevelance_in_females
+        disorder_dict["Population"] = population
+        disorder_dict["Continent"] = continent
+        all_disorders.append(disorder_dict)
 
-    # Clean up by closing the database cursor and connection
-    db_cursor.close()
-    db_conn.close()
+    return jsonify(all_disorders)
 
-# Moving the User Forward
-if __name__ == "__main__":
+
+#################################################
+# Run the App
+#################################################
+if __name__ == '__main__':
     app.run(debug=True)
