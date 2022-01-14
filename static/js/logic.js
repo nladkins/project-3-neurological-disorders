32993ad255e5d5d77d433732f45747ff3b4ce4cf
nladkins <adkins.norman@epa.gov>
2022-01-14 17:07:52 -0500

    Update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,19 @@
 url = "/json"
 
-d3.json(url).then(function(data) {
+function filtering(country) {
+  d3.json(url).then(function(data) {
+
+    // function selectCountry(row) {
+    //   return row.entity == country;
+    // }
 
     function selectADHD(row) {
-        return row.diagnosis == 'ADHD';
+        return ((row.diagnosis == 'ADHD') && (row.entity == country));
     }
     let adhdData = data.filter(selectADHD)
 
     console.log(adhdData)
+
     var ctx = adhdChart.getContext('2d');
     var xValues = adhdData.map(function(data) {
         return data.year
@@ -15,6 +21,8 @@ d3.json(url).then(function(data) {
     var yValues = adhdData.map(function(data) {
         return data.prevalence_in_females
     });
+    console.log(xValues)
+    console.log(yValues)
     new Chart(ctx, {
       type: "bar",
       data: {
@@ -27,7 +35,45 @@ d3.json(url).then(function(data) {
         legend: {display: false},
       }
     });
-});
+  });
+};
+
+// Need a function to identify the event that changed
+function optionChanged(country) {
+  filtering(country);
+}
+
+function init() {
+  // select the drop down portion of the html document
+  var dropdown = d3.select("#selDataset");
+
+// read the json data again pulling in the names for the drop down menu items
+  d3.json(url).then((data)=> {
+    console.log(data)
+    
+    var duplicateArray = []
+  // get the name data for the drop down items
+    data.forEach(function(row) {
+      duplicateArray.push(row.entity)
+    });
+
+    var uniqueArray = duplicateArray.filter(function(elem, pos) {
+      return duplicateArray.indexOf(elem) == pos;
+    });
+
+    uniqueArray.forEach(function(name) {
+      dropdown.append("option").text(name).property("value");
+    })
+
+  // using the name info to call out the function for the demographic info
+    // filtering(data.entity[0]);
+      
+  });
+
+}
+  // initialize
+  init();
+
 
 //    new Chart(document.getElementById("adhdChart"), {
 //        type: 'bar',
